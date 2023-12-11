4b26f3a7b4223b720ad149c19c1f9138ee58a467
Sardius <0xSardius@gmail.com>
2023-12-11 06:31:49 -0500

    update

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -1,14 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.18;
 
 import {Script, console2} from "forge-std/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
 
 contract DeployFundMe is Script {
-    function run() external returns (FundMe) {
+    function run() external {
         vm.startBroadcast();
-        FundMe fundMe = new FundMe();
+        new FundMe();
         vm.stopBroadcast();
-        return fundMe;
     }
 }
